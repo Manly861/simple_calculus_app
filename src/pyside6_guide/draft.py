612d411a0cda8af4856b219ff6f9438f066d5e3a
HippoMaiDinh <lym861@hsd.k12.or.us>
2026-04-07 13:32:58 -0700

    update draft

--- src/pyside6_guide/draft.py
+++ src/pyside6_guide/draft.py
@@ -28,16 +28,14 @@ class MainWindow(QMainWindow):
         title_label = QLabel("Welcome User To Calculus App!")
 
         # create a label 
-        instruction = QLabel("This is the basic Calculator App. Click The Button Below To Start!")
+        instruction = QLabel("This is the basic Calculator App. Enjoying It!")
         self.input_label = QLineEdit(placeholderText= "Enter A Number Here")
         self.answer = "Answer:"
         self.output_label = QLabel(self.answer)
 
-
         # create a button
         enter_button = QPushButton("=")
         enter_button.clicked.connect(self.process_input)
-
 
         # add widgets & layouts to main layout
         layout.addWidget(title_label)
@@ -57,21 +55,12 @@ class MainWindow(QMainWindow):
         
     def process_input(self):
         """process the input and mathematical function"""
-        if "+" in self.input_label.text() :
-            num_a = float(self.input_label.text()[ : (self.input_label.text().find("+"))])
-            num_b = float(self.input_label.text()[(self.input_label.text().find("+")) + 1 : ])
+        try:
+            answer = eval(self.input_label.text())
+            self.output_label.setText(f"Answer: {answer}")
+        except NameError and SyntaxError:
+            self.output_label.setText("ERROR!!!")
 
-            output = str(num_a + num_b)
-        elif self.plus_input_label.text() == "-":
-            output = str(num_a - num_b)
-        elif self.plus_input_label.text() == "x":
-            output = str(num_a * num_b)
-        elif self.plus_input_label.text() == "/":
-            output = str(num_a / num_b)
-        else:
-            output = "Warning"
-        
-        self.output_label.setText(f"Answer: {output}")
 
 
 if __name__ == "__main__":
