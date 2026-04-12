5f66e5a611cebc53f2ca257065f0707c5a1bb01d
HippoMaiDinh <lym861@hsd.k12.or.us>
2026-04-11 20:04:19 -0700

    fixing entering number 0

--- src/pyside6_guide/another_draft.py
+++ src/pyside6_guide/another_draft.py
@@ -15,47 +15,41 @@ class MyWindow(QWidget):
 
         # 1. Tạo ô nhập liệu
         self.line_edit = QLineEdit()
-        
+        btn0 = QPushButton("0")
+        btn0.setFixedSize(30,30)
+        btn0.clicked.connect(lambda: self.add_number("0"))
+ 
+        btn_enter = QPushButton("=")
+        btn_sum = QPushButton("+")
+        btn_subtract = QPushButton("-")
+        btn_multiply = QPushButton("x")
+        btn_divide = QPushButton("/")
+        btn_enter.clicked.connect(lambda: self.add_number("="))
+
         # 2. Tạo các nút bấm
         # for in in range
-        btn0 = QPushButton("0")
         for i in range(9):
             number_button = QPushButton(str(i+1))
             number_button.setFixedSize(30,30)
             number_button.clicked.connect(lambda _, x=str(i+1): self.add_number(x))
             if i < 3:
                 h_layout.addWidget(number_button)
+            elif i == 2:
+                h_layout.addWidget(btn_multiply)
             elif i < 6:
                 h_layout_1.addWidget(number_button)
+            elif i == 5:
+                h_layout_1.addWidget(btn_divide)
             else:
                 h_layout_2.addWidget(number_button)
 
-          
-
-
-
         # keep
-        btn_enter = QPushButton("=")
-        btn_percentage = QPushButton("%")
-        btn_enter.setFixedSize(30,30) 
-        btn_percentage.setFixedSize(30,30) 
-
-        # 3. Kết nối sự kiện (Dùng lambda để truyền giá trị)
-        # for in in range
-        # keep
-        btn_enter.clicked.connect(lambda: self.add_number("="))
+       
     
-
-        # 4. Layout
-        
-
-
-        # for in in range
 
         h_layout_3.addWidget(btn0)
         
         #keep
-        h_layout_3.addWidget(btn_percentage)
         h_layout_3.addWidget(btn_enter)
         
         layout.addWidget(self.line_edit)
